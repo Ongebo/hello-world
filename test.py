d0d051a1e77662fcd3f45967078c9fccca7599a3
Isaac Ongebo <isaacongebo@gmail.com>
2018-09-12 11:15:38 +0300

    Add second unit test

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ class HelloTests(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), 67)
 
+    def test_hello_world_with_wrong_return_value(self):
+        self.assertFalse(hello_world(), 78)
+
 
 if __name__ = '__main__':
     unittest.main()
